d1d562689cf2ec6d5624bfe806ed83dd05a300c8
EduardMLT <eddku@i.ua>
2024-02-02 18:52:10 +0200

    add Nav, Logo, banner

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./AppLayout";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
-const FavoritePage = lazy(() => import("./pages/FavoritePage/FavoritePage"));
+// const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
+// const FavoritePage = lazy(() => import("./pages/FavoritePage/FavoritePage"));
 
 
 export const App = () => {
@@ -13,8 +13,8 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="favorite" element={<FavoritePage />} />
-        <Route path="catalog" element={<CatalogPage />} />        
+        {/* <Route path="favorite" element={<FavoritePage />} />
+        <Route path="catalog" element={<CatalogPage />} />         */}
       </Route>
       <Route path="*" element={<HomePage />} />
     </Routes>
---
+++ src/components/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import { ArticleHomePage, DivTitle } from './HomePage.styled';
+// import MovingText from './HomePageAnimation';
+
+const HomePage = () => {
+  return (
+    <>
+      <ArticleHomePage>
+        <DivTitle>
+          Welcome <br /> Auto Rental Service
+        </DivTitle>
+        
+      </ArticleHomePage>
+    </>
+  );
+};
+
+export default HomePage;
